a8d3fdeeb64fd9fe7ab4a180157ce18c42a686d6
yernazarov <zhandos.yernazarov@nu.edu.kz>
2022-06-22 10:39:20 +0600

    Initialize project

---
+++ src/test/java/com/example/TourneyManager/TourneyManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TourneyManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourneyManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
